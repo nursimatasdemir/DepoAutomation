6cbf936798d1efa71d1e27e10b896a9254b06833
nursimatasdemir <nrsmtsdmr@gmail.com>
2025-11-11 15:08:10 +0300

    Missing unversioned files

---
+++ src/services/Identity/Identity.API/Program.cs
@@ -0,0 +1,56 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Identity.API.Services;
+using Identity.Application.Features.Auth.Commands.Register;
+using Identity.Domain;
+using Identity.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Identity.Application.Features.Auth.Commands.Register.Validation;
+using Identity.API.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<IdentityDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddIdentity<AppUser, IdentityRole<Guid>>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 4;
+})
+.AddEntityFrameworkStores<IdentityDbContext>()
+.AddRoles<IdentityRole<Guid>>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(RegisterCommand).Assembly)
+);
+
+builder.Services.AddValidatorsFromAssembly(typeof(RegisterCommand).Assembly);
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers().AddFluentValidationValidators();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
